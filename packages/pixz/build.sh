447663673d907239a56e79d82d292211aa60ea22
Matthew Klein <mklein994@gmail.com>
2016-11-03 11:49:01 -0500

    Add pixz package
    Pixz is a parallel xz compressor. Unlike `xz` from xz-utils, pixz can
    decompress files using multiple threads for faster performance.

---
+++ packages/pixz/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/vasi/pixz
+TERMUX_PKG_DESCRIPTION="Parallel, indexed xz compressor"
+TERMUX_PKG_VERSION=1.0.6
+TERMUX_PKG_DEPENDS="libarchive"
+TERMUX_PKG_SRCURL=https://github.com/vasi/pixz/releases/download/v${TERMUX_PKG_VERSION}/pixz-${TERMUX_PKG_VERSION}.tar.gz
