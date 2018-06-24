baa149db2eadfcb913b7083bd4036a88d0380197
Matthew Klein <mklein994@gmail.com>
2018-06-23 21:52:14 -0500

    Add sha256 checksum to pixz package

--- packages/pixz/build.sh
+++ packages/pixz/build.sh
@@ -2,4 +2,5 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vasi/pixz
 TERMUX_PKG_DESCRIPTION="Parallel, indexed xz compressor"
 TERMUX_PKG_VERSION=1.0.6
 TERMUX_PKG_DEPENDS="libarchive"
+TERMUX_PKG_SHA256=c54a406dddc6c2226779aeb4b5d5b5649c1d3787b39794fbae218f7535a1af63
 TERMUX_PKG_SRCURL=https://github.com/vasi/pixz/releases/download/v${TERMUX_PKG_VERSION}/pixz-${TERMUX_PKG_VERSION}.tar.gz
