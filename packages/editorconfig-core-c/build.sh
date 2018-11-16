01474a753867213c560d6b161b4c8ef6c99bdd48
Matthew Klein <mklein994@gmail.com>
2018-11-16 13:42:11 -0600

    Add an editorconfig-core-c package

---
+++ packages/editorconfig-core-c/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/editorconfig/editorconfig-core-c
+TERMUX_PKG_DESCRIPTION="EditorConfig core code written in C (for use by plugins supporting EditorConfig parsing)"
+TERMUX_PKG_VERSION=0.12.3
+TERMUX_PKG_SRCURL=https://github.com/editorconfig/editorconfig-core-c/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=64edf79500e104e47035cace903f5c299edba778dcff71b814b7095a9f14cbc1
+TERMUX_PKG_DEPENDS="pcre2"
+TERMUX_PKG_BUILD_DEPENDS="cmake"
