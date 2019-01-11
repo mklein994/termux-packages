680390906e1cfc8bdbc91bb7bab851f8e7e60b2e
Matthew Klein <mklein994@gmail.com>
2019-01-11 16:11:20 -0600

    cargo-cache: fix openssl-dev error
    Set `PKG_CONFIG_ALLOW_CROSS` to 1 fix an error compiling the
    `openssl-sys` crate.

--- disabled-packages/cargo-cache/build.sh
+++ disabled-packages/cargo-cache/build.sh
@@ -5,3 +5,9 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=efe077d72c9a64b47af63e426e3396ecd9f4ba40e46d4a1cbf16c859ec6e054a
 TERMUX_PKG_SRCURL=https://github.com/matthiaskrgr/cargo-cache/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_BUILD_DEPENDS="openssl-dev"
+
+termux_step_post_configure() {
+	export PKG_CONFIG_ALLOW_CROSS=1
+}
