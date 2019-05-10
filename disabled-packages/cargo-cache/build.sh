15b3316f1e74169beeb18c492e3cde000f7a7e29
Matthew Klein <mklein994@gmail.com>
2019-05-10 13:15:59 -0500

    cargo-cache: Add zlib-dev as a build dependency

--- disabled-packages/cargo-cache/build.sh
+++ disabled-packages/cargo-cache/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_SHA256=efe077d72c9a64b47af63e426e3396ecd9f4ba40e46d4a1cbf16c859ec6e05
 TERMUX_PKG_SRCURL=https://github.com/matthiaskrgr/cargo-cache/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_BUILD_DEPENDS="openssl-dev"
+TERMUX_PKG_BUILD_DEPENDS="openssl-dev, zlib-dev"
 
 termux_step_post_configure() {
 	export PKG_CONFIG_ALLOW_CROSS=1
