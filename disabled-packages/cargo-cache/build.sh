b35be7fd7270c5161b14d7c3cab5baabbb52487d
Matthew Klein <mklein994@gmail.com>
2019-05-21 13:39:37 -0500

    cargo-cache: Bump version to 0.2.0 and add licenses

--- disabled-packages/cargo-cache/build.sh
+++ disabled-packages/cargo-cache/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/matthiaskrgr/cargo-cache
 TERMUX_PKG_DESCRIPTION="Manage cargo cache (~/.cargo/), show sizes and remove directories selectively"
-TERMUX_PKG_VERSION=0.1.2
+TERMUX_PKG_VERSION=0.2.0
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=efe077d72c9a64b47af63e426e3396ecd9f4ba40e46d4a1cbf16c859ec6e054a
+TERMUX_PKG_LICENSE="Apache-2.0, MIT"
+TERMUX_PKG_SHA256=bcc78adafad130e5dca79a40e012308e9d28884017ff772b45e26df78a9657c9
 TERMUX_PKG_SRCURL=https://github.com/matthiaskrgr/cargo-cache/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="openssl"
